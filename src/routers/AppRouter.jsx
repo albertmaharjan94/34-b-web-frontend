ede649f91719c8ba2b42489ceed15ab31e992bbf
albert <albert.maharjan94@gmail.com>
2025-06-02 12:50:42 +0545

    jun 2

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -4,17 +4,19 @@ import Homepage from '../pages/Homepage'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import MainLayout from '../layouts/MainLayout'
+import StateManage from '../pages/StateManage'
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route element={<MainLayout/>}>
-                <Route path="/" element={<Homepage/>}></Route>
-                <Route path="/login" element={<Login/>}></Route>
-                <Route path='/register' element={<Register/>}></Route>
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path='/state-test' element={<StateManage />}></Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
